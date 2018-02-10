e14067ce49eaf020397d3f44d91ac99c58f3eb60
dendril <jorn91@gmail.com>
2018-02-10 10:31:50 +0100

    testing prints

--- training/KeyesHumTemp.py
+++ training/KeyesHumTemp.py
@@ -6,4 +6,7 @@ while True:
 
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 
+	print(temperature)
+	print(humidity)
+
     print ('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
