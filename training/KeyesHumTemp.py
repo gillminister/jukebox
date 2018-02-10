4839defb1ebcb428a59b198d70c3c6bc405643e9
dendril <jorn91@gmail.com>
2018-02-10 11:51:39 +0100

    create email notifier && rename tempHum->humTemp

--- training/KeyesHumTemp.py
+++ training/KeyesHumTemp.py
@@ -1,6 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import sys
 import Adafruit_DHT
+import smtplib
+
+
 
 # monitor check variables
 dangerReported = False
@@ -25,15 +29,13 @@ def handleDanger():
 
 # Monitor loop
 while True:
-
     tempHum = getTempHum()
     safeReport = checkTempHum(tempHum)
 
     if not safeReport['sys'] and not dangerReported:
         # Operating at non-optimal humidity / temperature ratio
         # Report and check immediately
-        print("nop")
-        dangerReported = True
+        notifiyDanger(tempHum)
 
     if dangerReported and safeReport['sys']:
         # False alarm
@@ -41,3 +43,37 @@ while True:
         dangerReported = False
 
     print ('Temp: {0:0.1f} C  Humidity: {1:0.1f} %  [ {2!s} ]'.format(tempHum['temp'], tempHum['hum'], safeReport['read']))
+
+
+#######################################################
+## ---------------- Email notifiers ---------------- ##
+#######################################################
+def notifiyDanger(humTemp):
+	# authenthicate gmail user
+	gmail_user = 'dendril.notifier@gmail.com'
+	gmail_password = 'S#Y^yV(rAk)7R*bw'
+
+	sent_from = gmail_user
+	to = ['jorn91@gmail.com', 'emiliehtakacs@gmail.com']
+	subject = 'Fuktig gang!'
+	body = 'Gangen hold no {0:0.1f}C {1:0.1f}% fukt. Sjekk måler og gjennomtrekk.'.format(humTemp['temp'], humTemp['hum'])
+
+	email_text = """\
+From: {0!s}
+To: {1!s}
+Subject: {2!s}
+
+{3!s}
+""".format(sent_from, ", ".join(to), subject, body)
+
+	try:
+	    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+	    server.ehlo()
+	    server.login(gmail_user, gmail_password)
+	    server.sendmail(sent_from, to, email_text)
+	    server.close()
+
+        dangerReported = True
+	    print 'SUCC:\tDanger notification email sent!'
+	except:
+	    print("ERR:\tcould not authenthicate gmail login")
